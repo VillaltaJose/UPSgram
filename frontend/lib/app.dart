628230e2ff8c6bb27a35ccbcebe1ddcda124a46f
DanielCollaguazo2003 <collaguazodaniel48@gmail.com>
2025-05-19 16:59:15 -0500

    feat: page route for navigation

---
+++ frontend/lib/app.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final _router = GoRouter(
+  initialLocation: '/feed',
+  routes: [
+    GoRoute(
+      path: '/feed',
+    ),
+  ],
+);
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: _router,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+    );
+  }
+}
